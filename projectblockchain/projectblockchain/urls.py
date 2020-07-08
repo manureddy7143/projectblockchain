e9e35bfa03a4f87b512b4754beee5e37c6d1cc3c
kumareddy14322 <60294666+kumareddy14322@users.noreply.github.com>
2020-07-08 20:09:35 +0530

    first page

--- projectblockchain/projectblockchain/urls.py
+++ projectblockchain/projectblockchain/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/', include('home.urls')),
-    path('admin/', admin.site.urls),]
+    path('', include('home.urls')),
+    path('', admin.site.urls),]
